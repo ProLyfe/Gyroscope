bf7cb73bd7dd0e5ee9d8e32f526ca935faa20090
Rayane Emamdee <rayane@MacBook-Pro-de-Rayane-2.local>
2023-05-03 15:21:08 +0200

    new gyro

--- main.js
+++ main.js
@@ -12,11 +12,36 @@ function handleOrientation(event) {
   var beta     = event.beta;
   var gamma    = event.gamma;
   // Do stuff with the new orientation data
-  setInterval(() => {
-    title2.innerHTML += `-${absolute}-${alpha}--${beta}--${gamma}-`
-  }, 1000)
 }
 
+let accelerometer = null;
+try {
+  accelerometer = new Accelerometer({ frequency: 10 });
+  accelerometer.onerror = (event) => {
+    // Handle runtime errors.
+    if (event.error.name === 'NotAllowedError') {
+      console.log('Permission to access sensor was denied.');
+    } else if (event.error.name === 'NotReadableError') {
+      console.log('Cannot connect to the sensor.');
+    }
+  };
+  accelerometer.onreading = (e) => {
+    console.log(e);
+    // setInterval(() => {
+      title2.innerHTML += `-${e}`
+    // }, 1000)
+  };
+  accelerometer.start();
+} catch (error) {
+  // Handle construction errors.
+  if (error.name === 'SecurityError') {
+    console.log('Sensor construction was blocked by the Permissions Policy.');
+  } else if (error.name === 'ReferenceError') {
+    console.log('Sensor is not supported by the User Agent.');
+  } else {
+    throw error;
+  }
+}
 
 let title = document.querySelector('.title');
 let title2 = document.querySelector('.title2');
